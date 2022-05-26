d9a56565b82be364b4825d04b5779d030b942270
LaffSteven <steve.laff@gmail.com>
2022-05-26 14:07:49 -0400

    created states

--- car_collector/src/App.js
+++ car_collector/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
+// import components for list of cars and individual cars
+import Cars from './components/Cars.js';
+import Car from './components/Car.js';
 
 const App = () => {
 
+    // state for car list from API call
+    const [carList, setCarList] = useState([]);
 
 
     return (
         <>
             <h1>Hello!</h1>
+            <Cars/>
         </>
     )
 }
