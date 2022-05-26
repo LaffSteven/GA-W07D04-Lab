e4af2c68c616b97c195981c265f1447e87941dd7
LaffSteven <steve.laff@gmail.com>
2022-05-26 14:32:28 -0400

    pass carList to Cars in props

--- car_collector/src/App.js
+++ car_collector/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
     return (
         <>
             <h1>Hello!</h1>
-            <Cars/>
+            <Cars cars={carList}/>
         </>
     )
 }
--- car_collector/src/components/Cars.js
+++ car_collector/src/components/Cars.js
@@ -3,10 +3,10 @@ import Car from './Cars.js'
 
 import {useState} from 'react';
 
-const Cars = () => {
+const Cars = (props) => {
 
     // state to hold list of cars from props
-    const [allCars, setAllCars] = useState([]);
+    const [allCars, setAllCars] = useState(props);
 
     return (
         <>
