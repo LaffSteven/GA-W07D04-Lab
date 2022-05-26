1a61183b836d4472717233a24e6a91f409b7c824
LaffSteven <steve.laff@gmail.com>
2022-05-26 12:32:52 -0400

    created basic components

---
+++ car_collector/src/components/Cars.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Car from './Cars.js'
+
+const Cars = (props) => {
+    return (
+        <>
+            List of Cars
+            <Car/>
+        </>
+    )
+
+}
+
+export default Cars
