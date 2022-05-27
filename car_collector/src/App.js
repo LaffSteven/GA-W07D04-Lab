3ab36fce7e45f3ae27a04e94559ad8042862325e
LaffSteven <steve.laff@gmail.com>
2022-05-26 21:39:01 -0400

    shows new car form and cancels to hide

--- car_collector/src/App.js
+++ car_collector/src/App.js
@@ -12,6 +12,15 @@ const App = () => {
     // state for car list from API call
     const [carList, setCarList] = useState([]);
 
+    // states for new car info
+    const [showNewCarForm, setShowNewCarForm] = useState(false);
+    const [newMake, setNewMake] = useState("");
+    const [newModel, setNewModel] = useState("");
+    const [newYear, setNewYear] = useState();
+    const [newColor, setNewColor] = useState("");
+    const [newImage, setNewImage] = useState("");
+    const [newWeekendCar, setNewWeekendCar] = useState(false);
+
     // get request api for cars on page load
     useEffect(() => {
         axios.get(`http://localhost:3000/cars`)
@@ -25,9 +34,54 @@ const App = () => {
         return <Cars cars={carList}/>
     }
 
+    const toggleShowNewCarForm = () => {
+        setShowNewCarForm(!showNewCarForm);
+        console.log(showNewCarForm);
+    }
+    const renderNewCarForm = () => {
+        return (
+            <>
+                <form onSubmit={handleNewCarSubmit}>
+                    Make: <input type="text" value={newMake} onChange={handleNewMakeChange}/>
+                    Model:
+                    Year:
+                    Image:
+                    Color:
+                    Weekend :
+                </form>
+                <button onClick={toggleShowNewCarForm}>Cancel</button>
+            </>
+        )
+    }
+    const handleNewMakeChange = (event) => {
+        setNewMake(event.target.value);
+    }
+    const handleNewModelChange = (event) => {
+        setNewModel(event.target.value);
+    }
+    const handleNewYearChange = (event) => {
+        setNewYear(event.target.value);
+    }
+    const handleNewImageChange = (event) => {
+        setNewImage(event.target.value);
+    }
+    const handleNewColorChange = (event) => {
+        setNewColor(event.target.value);
+    }
+    const handleNewCarSubmit = () => {
+
+    }
+
     return (
         <>
             <h1>Hello!</h1>
+            <div>
+                {showNewCarForm ?
+                    renderNewCarForm()
+                    :
+                    <button onClick={(event) => {toggleShowNewCarForm()}}>Create New Car</button>
+                }
+            </div>
             <>{renderCarList()}</>
         </>
     )
